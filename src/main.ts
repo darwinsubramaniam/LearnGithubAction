934932865cad3715e0b93e50a2ce4111c64e2595
Darwin Subramaniam <darwin.subramaniam@keysight.com>
2020-07-12 14:29:48 +0800

    bugfix for the console log error

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   console.log("darwin");
   console.log("minor");
+  console.log("minor release");
   await app.listen(3000);
 }
 bootstrap();
