03fe025addb4c43f616fc23dd0d1e45c985b8725
Darwin Subramaniam <darwin.subramaniam@keysight.com>
2020-07-12 14:25:30 +0800

    minor

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   console.log("darwin");
+  console.log("minor");
   await app.listen(3000);
 }
 bootstrap();
